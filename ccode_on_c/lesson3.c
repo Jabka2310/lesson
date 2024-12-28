ec44c4b1b70dee3f14881a2ad5acb03957fab36f
Никита <xris2002@list.ru>
2024-12-28 13:26:42 +0300

    Доки

---
+++ ccode_on_c/lesson3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+ char c;
+ scanf("%c", c);
+ printf("Hello, %c"); 
+  
+  
+  return 0;
+}
---
+++ ccode_on_c/lessontest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    string anser = get_string("what is your name\?\n");
+    printf("%c", &anser);
+
+
+    return 0;
+
+}
