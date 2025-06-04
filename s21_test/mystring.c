5ea9c9fe112fd5b33dc8538671d50b91a71163e8
Никита <xris2002@list.ru>
2025-06-04 07:19:36 +0300

    Директория для тестовых заданий по школе

---
+++ s21_test/mystring.c
@@ -0,0 +1,10 @@
+#include "mystring.h"
+
+size_t my_strlen(const char *str){
+  size_t lenght = 0;
+  while(*str != '\0'){
+    lenght++;
+    str++;
+  }
+  return lenght;
+}
---
+++ s21_test/mystring.h
@@ -0,0 +1,8 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+#include <stddef.h> // Для типа size_t
+
+size_t my_strlen(const char *str);
+
+#endif
