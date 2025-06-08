2f4b5004465f2a3effe354090dcf5736450654d0
Jabka2310 <tapok5@PC2.localdomain>
2025-06-08 20:32:20 +0300

    my_memchr

--- s21_string_test/mystring.h
+++ s21_string_test/mystring.h
@@ -6,5 +6,6 @@
 size_t my_strlen(const char *str);
 char *my_strcpy(char *dest, const char *src);
 int my_strcmp(const char *str1, const char *str2);
+void *my_memchr(const void *str, int c, size_t n);
 
 #endif
