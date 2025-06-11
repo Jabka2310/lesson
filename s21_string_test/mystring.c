d55179013248073361bc01c2b47400c59a0446eb
windwos-ubunta-1 <xris2002@list.ru>
2025-06-11 09:29:25 +0300

    strchr

--- s21_string_test/mystring.c
+++ s21_string_test/mystring.c
@@ -133,22 +133,20 @@ char *my_strncat(char *dest, const char *src,
   return dest;
 }
 
-char *my_strchr(const char *str, int c) {
-  if (str == NULL) {
-    return NULL;
-  }
-
-  while (*str != '\0') {
-    if (*str == (char)c) {
-      return (char *)str;
+char *my_strchr(const char *str, int c) { // Ищет первое вхождение символа c в строку str, возвращает указатель на этот символ или NULL если символ не найден
+    char *result = NULL;
+    
+    while (*str != '\0' && result == NULL) {
+        if (*str == (unsigned char)c) {
+            result = (char *)str;
+        }
+        str++;
     }
-    str++;
-  }
-
-  // Проверяем случай, когда c == '\0'
-  if ((char)c == '\0') {
-    return (char *)str;
-  }
-
-  return NULL;
+    
+    if (result == NULL && (unsigned char)c == '\0') {
+        result = (char *)str;
+    }
+    
+    return result;
 }
+
