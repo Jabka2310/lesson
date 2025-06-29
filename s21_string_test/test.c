957f3405e34fb4f458a2e236e7ec566320e9876c
Никита Христинчев <nikitahristincev@MacBook-Air-Nikita.local>
2025-06-29 19:42:52 +0300

    Первая функция

--- s21_string_test/test.c
+++ s21_string_test/test.c
@@ -2,11 +2,8 @@
 
 #include "mystring.h"
 
-int main() {
-  int str[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  my_memset(str, 0, 10);
-  for (int i = 0; i < 10; i++) {
-    printf("%d ", str[i]);
-  }
+int main(void){
+  char str[] = "Hello, world!";
+  printf("%s\n", s21_to_upper(str));
   return 0;
 }
