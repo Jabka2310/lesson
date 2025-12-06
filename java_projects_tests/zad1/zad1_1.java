9dd0666c88293ef849060fb105ffcedaec1e710f
windwos-ubunta-1 <xris2002@list.ru>
2025-12-07 01:18:06 +0300

    Чуть чуть начал копать джаву

---
+++ java_projects_tests/zad1/zad1_1.java
@@ -0,0 +1,9 @@
+
+public class zad1_1{
+
+  public static void main(String[] args){
+    System.out.println("Меня зовут Никита");
+    System.out.println("Мне 23 года");
+  }
+
+}
---
+++ java_projects_tests/zad1/zad1_2.java
@@ -0,0 +1,14 @@
+public class zad1_2{
+
+  public static void main(String[] args){
+
+    String name = "Никита";
+    int age = 23;
+    String city = "Ярославль";
+    
+    System.out.println("Имя:" + name);
+    System.out.println("Возраст:" + age);
+    System.out.println("Город:" + city);
+
+  }
+}
