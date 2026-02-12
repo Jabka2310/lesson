f88b666fce2288aac081bc6b2d795866c4b0f372
Никита Христинчев <nikitahristincev@MacBook-Air-Nikita.local>
2026-02-12 19:03:13 +0300

    4-e

---
+++ java_projects_tests/test/Z.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    float a = 50.22F;
+    float b = 1000000.00F;
+    printf("%f\n", a + b);
+    return 0;
+}
---
+++ java_projects_tests/test/Z.java
@@ -0,0 +1,7 @@
+public class Z {
+    public static void main(String[] args) {
+        float a = 50.22F;
+        float b = 1000000.00F;
+        System.out.println(a + b);
+    }
+}
